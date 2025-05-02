651cf4b1321e22c29ad5b21f9546de2ea90fab49
Maxim Karpukhin <50838885+karp013@users.noreply.github.com>
2025-05-02 18:04:39 +0300

    Feat: Добавлена поддержка fedora

--- toggle_gpu.sh
+++ toggle_gpu.sh
@@ -3,6 +3,23 @@
 # Путь к файлу с настройками модуля
 CONFIG_FILE="/etc/modprobe.d/blacklist-nouveau.conf"
 
+# Проверка, существует ли файл
+if [[ ! -f "$CONFIG_FILE" ]]; then
+    echo "Файл с настройками модуля не существует. Создаём файл..."
+    sudo touch "$CONFIG_FILE"
+    echo "Файл $CONFIG_FILE был создан."
+fi
+
+# Определение утилиты для обновления initramfs
+if [ -f /etc/debian_version ]; then
+    INITRAMFS_CMD="sudo update-initramfs -u"
+elif [ -f /etc/fedora-release ]; then
+    INITRAMFS_CMD="sudo dracut --force"
+else
+    echo "Неизвестная система. Не могу определить утилиту для обновления initramfs."
+    exit 1
+fi
+
 # Проверка текущего статуса
 if sudo grep -q "blacklist nouveau" "$CONFIG_FILE"; then
     echo "Сейчас видеокарта NVIDIA отключена."
@@ -15,7 +32,7 @@ if sudo grep -q "blacklist nouveau" "$CONFIG_FILE"; then
         sudo sed -i '/blacklist nouveau/d' "$CONFIG_FILE"
         sudo sed -i '/options nouveau modeset=0/d' "$CONFIG_FILE"
         echo "Обновление initramfs..."
-        sudo update-initramfs -u
+        $INITRAMFS_CMD
         echo "Видеокарта NVIDIA включена."
         echo "Перезагрузить систему для применения изменений? (Y/n)"
         read -r reboot_answer
@@ -37,7 +54,7 @@ else
         echo "Отключение видеокарты NVIDIA..."
         echo -e "blacklist nouveau\noptions nouveau modeset=0" | sudo tee "$CONFIG_FILE" > /dev/null
         echo "Обновление initramfs..."
-        sudo update-initramfs -u
+        $INITRAMFS_CMD
         echo "Видеокарта NVIDIA отключена."
         echo "Перезагрузить систему для применения изменений? (Y/n)"
         read -r reboot_answer
